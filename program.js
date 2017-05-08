52daed2cabf81cf72b54a89bd285159759f037b8
nattyait <nattanicha@gmail.com>
2017-05-08 14:39:33 +0700

    Filter LS

--- program.js
+++ program.js
@@ -1,8 +1,12 @@
+var path = require('path')
 var fs = require('fs')
 var number = undefined
 var content_split;
 
-fs.readFile(process.argv[2], function doneReading(err, data){
-	number = data.toString().split('\n').length - 1
-        console.log(number) 
+fs.readdir(process.argv[2], function doneReading(err, data){
+        for(var i=0; i<data.length; i++){
+          if(path.extname(data[i]).split('.')[1] == process.argv[3]){
+            console.log(data[i])
+          }
+        }
 })
